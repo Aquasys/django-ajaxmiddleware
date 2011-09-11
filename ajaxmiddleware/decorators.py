1544065230208bcb3668b44e157c70e30e8a2383
Adrien Lemaire <adrien.lemaire@aquasys.co.jp>
2011-09-11 22:26:50 +0000

    decorator improved to have exactly 4 tests added to decorated tests:
        * normal request get
        * normal request post
        * json request get
        * json request post

--- ajaxmiddleware/decorators.py
+++ ajaxmiddleware/decorators.py
@@ -10,19 +10,31 @@ def html_ajax_test(orig_test=None, url=None):
     """decorator used by tests to verify that http or json requests
     return the appropriate response"""
     def _decorated(test_func):
-        def _test(self):
-            # normal request
+        def _test_html_get(self):
             response = self.client.get(url)
             self.assertRaises(ValueError, json.loads, response.content)
             self.assertIsInstance(response, TemplateResponse)
             logger.info("\nhtml:\t%s" % ", ".join(response.template_name))
-            # ajax request
+
+        def _test_html_post(self):
+            pass
+
+        def _test_ajax_get(self):
             response = self.client.get(url, CONTENT_TYPE="application/json")
             self.assertNotIsInstance(response, TemplateResponse)
             self.assertIsInstance(json.loads(response.content), dict)
             logger.info("\njson:\t%s" % response.content)
             # Execute the rest of the test
             return test_func(self)
+
+        def _test_ajax_post(self):
+            pass
+
+        def _test(self):
+            _test_html_get(self)
+            _test_html_post(self)
+            _test_ajax_get(self)
+            _test_ajax_post(self)
 
         for attr in ["__name__", "__dict__", "__doc__"]:
             setattr(_test, attr, getattr(test_func, attr))
